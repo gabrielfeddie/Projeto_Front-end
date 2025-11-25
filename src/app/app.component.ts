d187e9cc1c02eaeb68f8e4089ef807b5334e1d45
Gabriel Felix <gabrielfelix099@hotmail.com>
2025-11-24 23:50:56 -0300

    Criação do component HelloWoeld.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './helloworld/helloworld.component' //Não é necessário colocar a extensão .ts - O sistema já identifica que é typescript
+// HelloWorldComponent - Nome da classe criada no dentro do arquivo helloworld.component.ts
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldComponent], //Importa o componente após a criação da classe na pasta do componente e importa deixa ele disponível para usar dentro do html.
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
