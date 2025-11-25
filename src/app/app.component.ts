aedce35c510431a4b369e515b8eea7e6b068c11f
Gabriel Felix <gabrielfelix099@hotmail.com>
2025-11-25 00:01:21 -0300

    Correções nos comentários

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HelloWorldComponent } from './helloworld/helloworld.component' //Não é necessário colocar a extensão .ts - O sistema já identifica que é typescript
+import { HelloWorldComponent } from './helloworld/helloworld.component' // Importa a classe do helloworld.component.ts. Não é necessário colocar a extensão .ts - O sistema já identifica que é typescript.
 // HelloWorldComponent - Nome da classe criada no dentro do arquivo helloworld.component.ts
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent], //Importa o componente após a criação da classe na pasta do componente e importa deixa ele disponível para usar dentro do html.
+  imports: [RouterOutlet, HelloWorldComponent], //Importa o componente após a criação da classe no arquivo do componente e importa deixa ele disponível para usar dentro do html.
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/helloworld/helloworld.component.ts
+++ src/app/helloworld/helloworld.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core' //Importa da bibliote Angular/core um Decorator chamado Component
 
-@Component({ // Decorator é uma classe que altera uma outra classe. Um componete que altera um componente original. A partir do momemnto que a classe "HelloWorldComponent" recebeu o componente, ele passou a ser um componente Angular
+@Component({ // Decorator é uma classe que altera uma outra classe. É um componente que altera um componente original. A partir do momento que a classe "HelloWorldComponent" recebeu o componente, ele passou a ser um componente Angular
     selector: 'hello-world', //Vai informar como esse componente é representado dentro de uma página html. Quando formos usar esse componente dentro de uma página HTML, vamos usar a tag <hello-world>
     template: '<h1> Hello, World!</h1>' //código html que aparecerá na página 
 
@@ -12,7 +12,7 @@ export class HelloWorldComponent{ //Exporta uma classe componente
 
 /* 
 
-Depois deve-se importar os componetes criados no app.component.ts
+Depois deve-se importar os componentes criados no app.component.ts
 
 
 */
